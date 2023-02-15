ec8d4c97c525e0d8401f5f41d4e0ad77020bfe85
JVRC22 <javier.res220704@gmail.com>
2023-02-15 00:41:10 -0600

    Migraciones tablas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->bigInteger('phone')->unique();
+            $table->foreignId('role')->references('id')->on('roles')->onUpdate('cascade')->onDelete('cascade')->nullable();
+            $table->bigInteger('code')->nullable();
+            $table->bigInteger('status')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2023_02_15_001653_create_propietarios_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('propietarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 20);
+            $table->string('ap_paterno', 20);
+            $table->string('ap_materno', 20);
+            $table->char('sexo');
+            $table->date('f_nac');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('propietarios');
+    }
+};
